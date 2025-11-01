98a4380fbc2d8f7dd5b5e9b77d57c6b2a2872506
FTC A <ftc18035@gmail.com>
2025-11-01 14:24:43 -0400

    Tweaked claw subsystems and started a basic Autonomous routine to learn the process

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/Attempt1NextFtcAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/Attempt1NextFtcAutonomous.java
@@ -1,63 +1,45 @@
 package org.firstinspires.ftc.teamcode.nextFTC;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-import static dev.nextftc.extensions.pedro.PedroComponent.follower;
+import org.firstinspires.ftc.teamcode.theclaw;
+import org.firstinspires.ftc.teamcode.theclawIntake;
 
 import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.components.BindingsComponent;
-import dev.nextftc.extensions.pedro.PedroComponent;
-import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.components.SubsystemComponent;
+
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
-import dev.nextftc.hardware.driving.MecanumDriverControlled;
-import dev.nextftc.hardware.impl.MotorEx;
-
 @Autonomous(name = "NextFTC Autonomous Program Java")
 public class Attempt1NextFtcAutonomous extends NextFTCOpMode {
-    public Attempt1NextFtcAutonomous() {
-        addComponents(
-             //   new SubsystemComponent(LiftSubSystem.INSTANCE),
-                new PedroComponent(Constants::createFollower),
-                BulkReadComponent.INSTANCE,
-                BindingsComponent.INSTANCE
-        );
-    }
+        public Attempt1NextFtcAutonomous() {
+            addComponents(
+                    new SubsystemComponent(theclaw.INSTANCE, theclawIntake.INSTANCE),
+                    BulkReadComponent.INSTANCE
+            );
+        }
 
-    // change the names and directions to suit your robot
-    private final MotorEx frontLeftMotor = new MotorEx("Front Left").reversed();
-    private final MotorEx frontRightMotor = new MotorEx("Front Right");
-    private final MotorEx backLeftMotor = new MotorEx("Back Left").reversed();
-    private final MotorEx backRightMotor = new MotorEx("Back Right");
+        private Command autonomousRoutine() {
+            return new SequentialGroup(
+                    theclaw.INSTANCE.close,
+                    new ParallelGroup(
+                            theclaw.INSTANCE.open,
+                            theclawIntake.INSTANCE.close
+                    ),
+                    new Delay(.5),
+                    new ParallelGroup(
+                            theclaw.INSTANCE.close,
+                            theclawIntake.INSTANCE.open
 
-  //  follower().breakFollowing();
+                    )
+            );
+        }
+
     @Override
     public void onStartButtonPressed() {
-        Command driverControlled = new MecanumDriverControlled(
-                frontLeftMotor,
-                frontRightMotor,
-                backLeftMotor,
-                backRightMotor,
-                Gamepads.gamepad1().leftStickY().negate(),
-                Gamepads.gamepad1().leftStickX(),
-                Gamepads.gamepad1().rightStickX()
-        );
-        driverControlled.schedule();
-
-       /* Gamepads.gamepad2().dpadUp()
-                .whenBecomesTrue(
-                        LiftSubSystem.INSTANCE.toHigh
-                );
-
-        Gamepads.gamepad2().rightTrigger().greaterThan(0.2)
-                .whenBecomesTrue(
-                        LiftSubSystem.INSTANCE.toMiddle
-                );
-
-        Gamepads.gamepad2().leftBumper().whenBecomesTrue(
-                        LiftSubSystem.INSTANCE.toLow
-                );*/
+        autonomousRoutine().schedule();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/theclawIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/theclawIntake.java
@@ -6,13 +6,11 @@ import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.hardware.impl.ServoEx;
 import dev.nextftc.hardware.positionable.SetPosition;
 
-public class theclaw implements Subsystem{
+public class theclawIntake implements Subsystem{
     // Hannah and Grace
-    public static final theclaw INSTANCE = new theclaw();
-    private theclaw() { }
-
-    private ServoEx servo = new ServoEx("Outtake Claw");
+    public static final theclawIntake INSTANCE = new theclawIntake();
+    private theclawIntake() { }
+    private ServoEx servo = new ServoEx("Intake Claw");
     public Command open = new SetPosition(servo, 0).requires(this);
     public Command close = new SetPosition(servo, .45).requires(this);
-
 }
