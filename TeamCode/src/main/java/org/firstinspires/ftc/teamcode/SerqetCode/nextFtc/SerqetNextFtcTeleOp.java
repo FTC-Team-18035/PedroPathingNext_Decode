1eec1bffdc66a9a7f5564de72e61bbe7a52c3838
Jan Martin <janrmartin1@gmail.com>
2025-11-25 07:40:14 -0800

    SerqetNextFtcTeleOp with INTAKE/VAULT/SHOOTER/LIFT systems enabled but no sensor inputs (only passing final values as set in MAIN)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/SerqetNextFtcTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/SerqetNextFtcTeleOp.java
@@ -11,7 +11,6 @@ import dev.nextftc.bindings.BindingManager;
 import dev.nextftc.bindings.Button;
 import dev.nextftc.bindings.Variable;
 import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.CommandManager;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.Gamepads;
@@ -19,18 +18,38 @@ import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 import dev.nextftc.hardware.driving.DriverControlledCommand;
 import dev.nextftc.hardware.driving.MecanumDriverControlled;
+import dev.nextftc.hardware.impl.Direction;
+import dev.nextftc.hardware.impl.IMUEx;
 import dev.nextftc.hardware.impl.MotorEx;
 import static dev.nextftc.bindings.Bindings.*;
 
+/*  Current MAIN teleop status:
+
+    Drivetrain is active inside MAIN and should be moved to a Subsystem
+    Robotcentric is current contol mode
+
+    INTAKE is active
+    VAULT is active
+    SHOOTER is active
+
+    Control layout (all are gamepad1)  TODO - test and get driver feedback for layout
+
+    left_trigger is variable TurboMode
+    right_bumper is INTAKE
+    a is SHOOT
+*/
+
+
 
 @TeleOp(name = "Serqet Lift/Intake/Vault TeleOp")
+
 public class SerqetNextFtcTeleOp extends NextFTCOpMode {
     public SerqetNextFtcTeleOp() {
         addComponents(
              new SubsystemComponent(LiftSubsystem.INSTANCE),    // enable LIFT system
              new SubsystemComponent(IntakeSubsystem.INSTANCE),    // enable INTAKE system
              new SubsystemComponent(VaultSubsystem.INSTANCE),    // enable VAULT system
-                // new SubsystemComponent(ShooterSubSystem.INSTANCE),    // enable SHOOTER system
+             new SubsystemComponent(Shooter.INSTANCE),    // enable SHOOTER system
 
                 BulkReadComponent.INSTANCE,
                 BindingsComponent.INSTANCE
@@ -43,17 +62,17 @@ public class SerqetNextFtcTeleOp extends NextFTCOpMode {
     private final MotorEx frontRight = new MotorEx("front_right");
     private final MotorEx backLeft = new MotorEx("back_left").reversed();
     private final MotorEx backRight = new MotorEx("back_right");
+    private IMUEx pinpoint = new IMUEx("pinpoint", Direction.UP, Direction.FORWARD).zeroed();    // needed for Pedro field centric
+    public double dtScale = 0.5; // Drivetrain scalar variable to set default to half power
 
-    // private IMUEx imu = new IMUEx("imu", Direction.UP, Direction.FORWARD).zeroed()    // needed for Pedro field centric
-    public double dtScale = 1;
-
+    // Actions to take when opmode is INITIALIZED
     @Override
     public void onInit()  {                     // set LIFT to hold the hold the lift
         final Command holdClear = LiftSubsystem.INSTANCE.holdClear;
         holdClear.schedule();
     }
 
-
+    // Actions to take/ button bindings when START is pressed
     @Override
     public void onStartButtonPressed() {
         telemetry.addData("", telemetryValue);
@@ -69,40 +88,38 @@ public class SerqetNextFtcTeleOp extends NextFTCOpMode {
                 Gamepads.gamepad1().rightStickX()
                 // new HolonomicMode.FieldCentric(imu)  // needed for Pedro field centric
         );
-
-        Variable<Float> left_trigger = variable(() -> gamepad1.left_trigger);
-        Button turboButton = left_trigger.asButton(value -> value > 0.5); // true when left trigger is positive.
-
-        turboButton // TURBO mode but it's variable // change to whiletrue if value does not update
-                .whenTrue(() -> dtScale = (gamepad1.left_trigger))
-                .whenBecomesFalse(() -> dtScale = 0.5);             // default to half power
-
         driverControlled.setScalar(dtScale);
         driverControlled.schedule();
 
-        Gamepads.gamepad1().dpadUp().whenBecomesTrue(
-                        LiftSubsystem.INSTANCE.toHigh
-                );                                          // Bind LIFT activation to button
-
-        //TODO Figure out Trajectory. We need Limelight feedback for trajectory and shooting.
-        //TODO Have PinPoint hold position Pedro?
-        //TODO Hit Shoot use limelight for feedback, if we get pushed move back, then shoot
-        Gamepads.gamepad1().a().whenBecomesTrue(
-                        Shooter.INSTANCE.shoot(1)              // Here could be the commands to hold position via Pinpoint and Pedro?
-                ).whenBecomesFalse(
-                        Shooter.INSTANCE.stop
-                );                                                  // Bind SHOOTER activation to button
-
-        Gamepads.gamepad1().rightBumper().whenTrue(
-                       IntakeSubsystem.INSTANCE.run.and(
-                       VaultSubsystem.INSTANCE.intake)
-                );                                          // Bind INTAKE/VAULT run operation to button
-        Gamepads.gamepad1().rightBumper().whenFalse(
-                        IntakeSubsystem.INSTANCE.stop.and(
-                        VaultSubsystem.INSTANCE.stop)
-                );                                          // Bind INTAKE/VAULT stop operation to button
+        // Bind TurboMode to left trigger
+        // this is trickier since the trigger(supplier) isn't a boolean
+        Variable<Float> left_trigger = variable(() -> gamepad1.left_trigger);
+        Button turboButton = left_trigger.asButton(value -> value > 0.5); // true when left trigger is positive.
+        turboButton                                                     // TURBO mode but it's variable
+                .whenTrue(() -> dtScale = (gamepad1.left_trigger))      // Lambda command to set trigger value to be the drivetrain scalar value
+                .whenBecomesFalse(() -> dtScale = 0.5);                 // default to half power
 
 
+        // Bind shooting actions to gampad1.a
+        button(() -> gamepad1.a)
+                .whenBecomesTrue(Shooter.INSTANCE.spinup)               // may not be helpful - a delay in shoot command (Subsystem level) may be best
+                .whenTrue(Shooter.INSTANCE.shoot(5,.1, 0)
+                        .then(VaultSubsystem.INSTANCE.outtake))
+                // TODO - how to trigger Limelight read/Trajectory calculation and pass
+                // TODO - Have PinPoint hold position Pedro?
+                .whenBecomesFalse(VaultSubsystem.INSTANCE.stop)
+                .whenFalse(Shooter.INSTANCE.stop);
+
+        // Bind LIFT activation to button
+        button(() -> gamepad1.dpad_up)
+                .whenBecomesTrue(LiftSubsystem.INSTANCE.toHigh);        // Parking action to raise lift
+
+        // Bind INTAKE actions to button
+        button(() -> gamepad1.right_bumper)
+                .whenTrue(IntakeSubsystem.INSTANCE.run
+                    .and(VaultSubsystem.INSTANCE.intake))               // activate INTAKE and VAULT for getting artifacts
+                .whenFalse(IntakeSubsystem.INSTANCE.stop
+                    .and(VaultSubsystem.INSTANCE.stop));                // de-activate INTAKE and VAULT
     }
 
 
@@ -110,14 +127,21 @@ public class SerqetNextFtcTeleOp extends NextFTCOpMode {
     @Override
     public void onUpdate() {            // code to run during loop()
         // in loop(), or in NextFTC, onUpdate():
-        BindingManager.update();
+        BindingManager.update();        // this is what checks for the gamepad input during loop
 
     }
 
     @Override
     public void onStop(){               // code to run once on stop()
         // in stop(), or in NextFTC, onStop():
-        BindingManager.reset();
+        BindingManager.reset();         // this is just housekeeping at the end of teleOp
     }
 
+    // TODO - add sensor methods here (pinpoint/Limelight)
+    // private void Limelight3A() {
+    //
+    // }
+    // private void pinpoint() {
+    //
+    // }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/subsystems/Shooter.java
@@ -1,28 +1,54 @@
 package org.firstinspires.ftc.teamcode.SerqetCode.nextFtc.subsystems;
 
+import dev.nextftc.control.ControlSystem;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.hardware.controllable.MotorGroup;
 import dev.nextftc.hardware.controllable.RunToVelocity;
 import dev.nextftc.hardware.impl.MotorEx;
-import dev.nextftc.hardware.powerable.SetPower;
+import dev.nextftc.hardware.impl.ServoEx;
+import dev.nextftc.hardware.positionable.SetPosition;
+import org.firstinspires.ftc.teamcode.SerqetCode.Trajectory;
 
 public class Shooter implements Subsystem {
 
     public static final Shooter INSTANCE = new Shooter();
-
+    public static double targetDistance = 0;
     private Shooter() { }
 
-    MotorGroup shooterGroup = new MotorGroup(
+    // Declare servos and motors
+    private final ServoEx servoVertical = new ServoEx("shooter_vertical");
+    private final ServoEx servoHorizontal = new ServoEx("shooter_horizontal");
+    private MotorGroup shooterGroup = new MotorGroup(
             new MotorEx("left_shooter"),
-            new MotorEx("right_shooter").reversed()
-    );
+            new MotorEx("right_shooter").reversed());
 
+    // PID for the shooter motors
+    // TODO - program servos
+    // TODO - verify motor directions
+    // TODO - tune PID values
+    private ControlSystem controller = ControlSystem.builder()
+            .velPid(0.0001, 0, 0.001)
+            .basicFF(0.001,0,0)
+            .build();
 
-    public Command shoot(double trajectory) {
-        return new SetPower(shooterGroup, trajectory).requires(this);
+    // command to call when aiming and shooting action is attempted
+    public Command shoot(double launchVelocity, double launchAngle, double aimAngle) {                   // feed calculated values into motor control and servos
+        return new SetPosition(servoHorizontal,aimAngle).requires(servoHorizontal)          // servo angle adjustment
+                .and(new SetPosition(servoVertical, launchAngle).requires(servoVertical))   // may need a delay here ???
+                .then(new RunToVelocity(controller, launchVelocity).requires(this));
+    }
+    // command to spin up shooter motors
+    public Command spinup = new RunToVelocity(controller,.5).requires(this).thenWait(0.1);  // initial spin up command (with delay) that may not be neede
+    // command to stop
+    public Command stop = new RunToVelocity(controller, 0).requires(this);  // stop shooter motors by setting controller goal to 0
+
+    @Override
+    public void periodic(){
+        shooterGroup.setPower(controller.calculate(shooterGroup.getState()));    // update the controller calculations with each loop iteration
     }
 
-
-    public Command stop = new SetPower(shooterGroup, 0).requires(this);
 }
+
+
+
