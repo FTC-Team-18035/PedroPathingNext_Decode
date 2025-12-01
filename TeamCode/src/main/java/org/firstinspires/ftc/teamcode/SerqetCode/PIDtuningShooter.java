cc0c0c5ddb095d06875f621b791fb032368649ef
Jan Martin <janrmartin1@gmail.com>
2025-11-30 17:14:36 -0800

    Shooter PID Velocity tuning opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/PIDtuningShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/PIDtuningShooter.java
@@ -1,69 +1,72 @@
 package org.firstinspires.ftc.teamcode.SerqetCode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
 import com.bylazar.configurables.annotations.Configurable;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-import org.firstinspires.ftc.teamcode.SerqetCode.nextFtc.subsystems.Shooter;
 
 @Configurable
-@TeleOp
+@TeleOp (name = "Shooter Tuner FTClib")
 public class PIDtuningShooter extends OpMode {
+    public PIDtuningShooter() {    }
 
-    private PIDController ShooterController;
+    private PIDFController ShooterController;
 
-    public static double sp = 0,
-            si = 0, sd = 0;
-
-    public static double sf = 0;
-
-    public static int shooterTarget = 0;
-
+    public static double sp = 0, si = 0, sd = 0, sf = 0;
+    public static double shooterTarget = 0;
+    public static double shooterVel = 0;
+    public static double shooterPower = 0;
     private final double shooter_ticks_in_degrees = 0;
 
-    private DcMotorEx LeftShooter;
-
+    private DcMotorEx leftShooter;
+    private DcMotorEx rightShooter;
 
     @Override
-    public void init(){
-        ShooterController = new PIDController(sp, si, sd);
+    public void init() {
+        ShooterController = new PIDFController(sp, si, sd, sf);
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        LeftShooter = hardwareMap.get(DcMotorEx.class, "left_shooter");
+        leftShooter = hardwareMap.get(DcMotorEx.class, "left_shooter");
+        rightShooter = hardwareMap.get(DcMotorEx.class, "right_shooter");
 
-        LeftShooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftShooter.setDirection(DcMotorSimple.Direction.REVERSE);  // reverse one motor
 
-        LeftShooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftShooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightShooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftShooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightShooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
     }
 
-    @Override
     public void loop(){
-        ShooterController.setPID(sp, si, sd);
+        ShooterController.setPIDF(sp, si, sd, sf);          // use new PIDF values
 
+        double shooterVel = leftShooter.getVelocity();      // read velocity
 
-        int shooterPos = LeftShooter.getCurrentPosition();
+        double sPID = ShooterController.calculate(shooterVel, shooterTarget);   // calculate path to new velocity target
 
-        double sPID = ShooterController.calculate(shooterPos, shooterTarget);
+        double shooterFF = Math.cos(Math.toRadians(shooterTarget / shooter_ticks_in_degrees)) * sf;  // more math
 
+        double shooterPower = sPID + shooterFF;             // calculate corrected power value
 
-        double shooterFF = Math.cos(Math.toRadians(shooterTarget / shooter_ticks_in_degrees)) * sf;
+        leftShooter.setPower(shooterPower);                 // set power to motors
+        rightShooter.setPower(shooterPower);
 
-        double shooterPower = sPID + shooterFF;
-
-        LeftShooter.setPower(shooterPower);
-
-        telemetry.addData("Lift pos ", shooterPos);
-        telemetry.addData("lift target ", shooterTarget);
+        telemetry.addData("Shooter Velocity", shooterVel);      // output to Driver Station
+        telemetry.addData("Shooter Target ", shooterTarget);
         telemetry.update();
+
 
     }
 
