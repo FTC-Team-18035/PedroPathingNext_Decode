f1a3aaeaa2e0b33fd2d9c9b4afe116fc6ca32781
Jan Martin <janrmartin1@gmail.com>
2025-12-04 22:39:51 -0500

    Updated Shooter tuning file for Panels intergration also

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/PIDFtuningShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/PIDFtuningShooter.java
@@ -1,12 +1,9 @@
 package org.firstinspires.ftc.teamcode.SerqetCode;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDFController;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.arcrobotics.ftclib.hardware.motors.MotorEx;
-import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
 import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -16,25 +13,20 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Configurable
 @TeleOp (name = "Shooter Tuner FTClib")
-public class PIDtuningShooter extends OpMode {
-    public PIDtuningShooter() {    }
+public class PIDFtuningShooter extends OpMode {
+    public PIDFtuningShooter() { }
 
     private PIDFController ShooterController;
-
     public static double sp = 0, si = 0, sd = 0, sf = 0;
     public static double shooterTarget = 0;
-    public static double shooterVel = 0;
-    public static double shooterPower = 0;
     private final double shooter_ticks_in_degrees = 0;   // TODO - set this for specific motors
-
     private DcMotorEx leftShooter;
     private DcMotorEx rightShooter;
+    public static TelemetryManager panelsTelemetry;
 
     @Override
     public void init() {
         ShooterController = new PIDFController(sp, si, sd, sf);
-
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         leftShooter = hardwareMap.get(DcMotorEx.class, "left_shooter");
         rightShooter = hardwareMap.get(DcMotorEx.class, "right_shooter");
@@ -46,6 +38,8 @@ public class PIDtuningShooter extends OpMode {
 
         leftShooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightShooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry(); // Panels telemetry interface
 
     }
 
@@ -60,13 +54,15 @@ public class PIDtuningShooter extends OpMode {
 
         double shooterPower = sPID + shooterFF;             // calculate corrected power value
 
+        double error = shooterTarget - shooterVel;
+
         leftShooter.setPower(shooterPower);                 // set power to motors
-        rightShooter.setPower(shooterPower);
+        rightShooter.setPower(-shooterPower);               // TODO - verify motor directions !!!
 
-        telemetry.addData("Shooter Velocity", shooterVel);      // output to Driver Station
-        telemetry.addData("Shooter Target ", shooterTarget);
-        telemetry.update();
-
+        panelsTelemetry.debug("Shooter Velocity" + shooterVel);      // output to Panels
+        panelsTelemetry.debug("Shooter Target " + shooterTarget);
+        panelsTelemetry.debug("error " + error + " !");
+        panelsTelemetry.update();
 
     }
 
