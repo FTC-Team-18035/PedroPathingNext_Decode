f93fd2e0244dc4deab75c15b7beba34bfc78c349
Jan Martin <janrmartin1@gmail.com>
2025-12-05 07:43:09 -0500

    Added feedback feature TODO for VAULT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/subsystems/Vault.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/subsystems/Vault.java
@@ -9,16 +9,18 @@ import dev.nextftc.hardware.powerable.SetPower;
 
 public class Vault implements Subsystem {
 
-    /* This creates a public class that allows any opmode access to our VAULT system
-       All functions of this subsystem are controlled by this one class of code and can be referenced by both auto and tele modes
-       through the calling of the predefined Commands below.  Any actions or adjustments to the operation of the INTAKE should take
-       place within this single class
-    */
+    /*
+     * This creates a public class that allows any opmode access to our VAULT system
+     * All functions of this subsystem are controlled by this one class of code and can be referenced by both auto and tele modes
+     * through the calling of the predefined Commands below.  Any actions or adjustments to the operation of the INTAKE should take
+     * place within this single class
+     */
 
     public static final Vault INSTANCE = new Vault();     // public INSTANCE of this class to give access
     private Vault() {}                                    // private object of this class
 
     // servos used by this subsystem
+    // TODO - enhance with FeedbackCRServoEx - current NextFTC instructions are missing
     private CRServoEx vaultFeed = new CRServoEx( "vault_feed");
     private ServoEx vaultRelease = new ServoEx( "vault_release");
 
