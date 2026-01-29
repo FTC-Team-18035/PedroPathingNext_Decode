92fab16e5282eabbe1fcb2a23389292563bb48ca
FTC A <ftc18035@gmail.com>
2026-01-28 20:30:04 -0500

    Working hold position in autonomous. Still need to work on using it during TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/JustHoldPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/JustHoldPoint.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.SerqetCode.nextFtc;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -11,50 +12,98 @@ import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.HeadingInterpolator;
 import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.SerqetCode.nextFtc.pedroPathing.Constants;
+
+@Configurable
 @TeleOp
 public class JustHoldPoint extends OpMode {
-
+    public boolean holdInitialized;
+    public boolean holdStart;
+    public double holdHeadingRad;
+    public static final double HOLD_HEADING_KP = 0.45;
+    public static final double HOLD_MAX_TURN = 0.45;
+    public static final double HOLD_MIN_TURN_CMD = 0.06;
+    public static final double HOLD_HEADING_DEADBAND_RAD = Math.toRadians(1.0);
+    public BezierPoint holdPoint = new BezierPoint(0, 0);
     private Follower follower;
-    private Pose  endPose, middlePose;
-    private BezierPoint startPose;
+    private Pose  endPose, middlePose, startPose;
+    //private BezierPoint startPose;
     private PathChain holdPath;
+    private Path drive;
+    private double turn = 0;
 
     @Override
     public void init() {
-        startPose = new BezierPoint(new Pose(0, 0, 0));
-        middlePose = new Pose(0.5, 0, 0);
-        endPose = new Pose(0, 0, 0);
-        MecanumConstants mecanumConstants = new MecanumConstants()
-                .leftFrontMotorName("front_left")
-                .leftRearMotorName("back_left")
-                .rightFrontMotorName("front_right")
-                .rightRearMotorName("back_right");
-        PinpointConstants pinpointConstants = new PinpointConstants()
-                .hardwareMapName("pinpoint");
-        follower = new FollowerBuilder(new FollowerConstants(), hardwareMap)
-                .mecanumDrivetrain(mecanumConstants)
-                .pinpointLocalizer(pinpointConstants)
-                .build();
-
-      /*  holdPath = follower.pathBuilder()
-                .addPath(new BezierLine(startPose, middlePose))
-                .setLinearHeadingInterpolation(startPose.getHeading(), middlePose.getHeading())
-                .addPath(new BezierLine(middlePose, endPose))
-                .setLinearHeadingInterpolation(middlePose.getHeading(), endPose.getHeading())
-                .build();*/
-
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(0, 0, 0));
+        holdInitialized = false;
     }
 
     @Override
     public void start(){
-      //  follower.followPath();
+        follower.update();
+        //beginHoldFromCurrentPose();
     }
     @Override
     public void loop() {
-        follower.update();
+        if(gamepad1.a) {
+            holdStart = false;
+            follower.setTeleOpDrive(
+                    0,
+                    0,
+                    0,
+                    false,
+                    0
+            );
+        }
+        else if(gamepad1.yWasPressed()) {
+            holdInitialized = false;
+            updateHold();
+            follower.update();
+            holdStart = true;
+        }
+        if(holdStart){
+            updateHold();
+            follower.update();
+        }
+    }
+    private void beginHoldFromCurrentPose() {
+        if (holdInitialized) return;
+        holdPoint = new BezierPoint(follower.getPose().getX(), follower.getPose().getY());
+        holdHeadingRad = follower.getPose().getHeading();
+        holdInitialized = true;
+    }
 
+    private void updateHold() {
+        if (!holdInitialized) {
+            beginHoldFromCurrentPose();
+        }
+
+        double headingError = angleWrapRad(holdHeadingRad - follower.getPose().getHeading());
+        turn = HOLD_HEADING_KP * headingError;
+
+        if (Math.abs(headingError) < HOLD_HEADING_DEADBAND_RAD) {
+            turn = 0.0;
+        } else {
+            if (Math.abs(turn) < HOLD_MIN_TURN_CMD) {
+                turn = Math.copySign(HOLD_MIN_TURN_CMD, turn);
+            }
+            turn = clamp(turn, -HOLD_MAX_TURN, HOLD_MAX_TURN);
+        }
+
+        follower.holdPoint(holdPoint, turn);
+    }
+
+    private static double clamp(double value, double min, double max) {
+        return Math.max(min, Math.min(max, value));
+    }
+    private static double angleWrapRad(double radians) {
+        while (radians > Math.PI) radians -= 2.0 * Math.PI;
+        while (radians < -Math.PI) radians += 2.0 * Math.PI;
+        return radians;
     }
 }
