456720dc658f1d81fa53740ede91d0f998b3009b
CertifiedNerdy <eli.r.keeler@gmail.com>
2026-02-13 18:06:40 -0500

    UPDATED Lift motor was still set to RUN_TO_POSITION, changed to RUN_WITHOUT_ENCODER manual control with auto shutdown @3600 encoder reading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/REDMainTeleOpWORKING.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SerqetCode/nextFtc/REDMainTeleOpWORKING.java
@@ -122,7 +122,7 @@ public class REDMainTeleOpWORKING extends LinearOpMode {
         lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         lift.setTargetPosition(0);
-        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(8); // AprilTag pipeline
@@ -354,9 +354,11 @@ public class REDMainTeleOpWORKING extends LinearOpMode {
     }
 
     private void handleLift() {
-        if(gamepad1.dpad_up && gamepad1.left_trigger > .75) {    //TODO Changed it so you have to be holding the left trigger to run the lift
-            lift.setTargetPosition(3600);
+        if(gamepad1.dpad_up && gamepad1.left_trigger > .75 && lift.getCurrentPosition() < 3600) {    //TODO Changed it so you have to be holding the left trigger to run the lift
             lift.setPower(1);
+        }
+        else {
+            lift.setPower(0);
         }
     }
 }
